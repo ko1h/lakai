1f9576d04a1f7346cdddce52b4b75fea0fbec377
Kohichi Horii <kohichihorii@Kohichis-MacBook-Pro.local>
2019-03-01 16:31:29 -0800

    Add new about.html page with header and footer

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,12 @@ module.exports = {
       inject: 'body',
       template: './src/index.html',
       filename: 'index.html',
-    })
+    }),
+
+    new HtmlWebpackPlugin({
+   template: './src/about.html',
+   filename: 'about.html'
+  }),
 
   ]
 
